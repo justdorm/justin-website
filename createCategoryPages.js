774724a31758e6ab31ac6060b3698c750dfc49ad
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-02-12 16:27:37 -0700

    Automatically delete old category pages

--- createCategoryPages.js
+++ createCategoryPages.js
@@ -13,6 +13,17 @@ var kebabCase = function(word) {
     return word.toLowerCase().replace(/[\s_]/g, '-');
 };
 
+console.log("Cleaning outdated files...");
+
+fs.readdirSync("src").forEach(function(file) {
+    if (path.extname(file) !== ".html") return;
+    file = path.join(".", "src", file);
+    if (fs.readFileSync(file, 'utf8').match(/\<\!\-\- Automatically generated on /)) {
+        fs.unlinkSync(file);
+        console.log("Deleted " + file);
+    }
+});
+
 console.log("Creating category pages...");
 
 for (var category in portfolio) {
@@ -21,6 +32,7 @@ for (var category in portfolio) {
     }
     var categoryFile = path.join(".", "src", kebabCase(category) + ".html");
     var categoryHtml = "<!--\ntitle: " + category + "\ncategory: " + category + "\n-->";
+    categoryHtml += "\n<!-- Automatically generated on " + new Date().toUTCString() + " -->";
     fs.writeFileSync(categoryFile, categoryHtml);
     console.log("Wrote " + categoryFile + ".");
 }
