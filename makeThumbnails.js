2c9622d5185e0e694b3891a17bf1407b7b4a428a
Jeremy Dormitzer <jdormitzer@hubspot.com>
2018-01-09 07:18:22 -0500

    Make large thumbnails for index page

--- makeThumbnails.js
+++ makeThumbnails.js
@@ -3,22 +3,26 @@ var path = require("path");
 var sharp = require("sharp");
 var mkdirp = require("mkdirp");
 
-mkdirp(path.join(".", "src", "thumbnails"));
-
-fs.readdir(path.join(".", "src", "images"), function(err, files) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  files.forEach(function(file) {
-    console.log("Making thumbnail for %s", file);
-    sharp(path.join(".", "src", "images", file))
-      .resize(230)
-      .toFile(path.join(".", "src", "thumbnails", path.basename(file)), function(err) {
+function thumbnailicize(targetSize, targetDir) {
+    mkdirp(path.join(".", "src", targetDir));
+    fs.readdir(path.join(".", "src", "images"), function(err, files) {
         if (err) {
-          console.error(err);
-          process.exit(1);
+            console.error(err);
+            process.exit(1);
         }
-      });
-  });
-});
+        files.forEach(function(file) {
+            console.log("Making %d px thumbnail for %s", targetSize, file);
+            sharp(path.join(".", "src", "images", file))
+                .resize(targetSize)
+                .toFile(path.join(".", "src", targetDir, path.basename(file)), function(err) {
+                    if (err) {
+                        console.error(err);
+                        process.exit(1);
+                    }
+                });
+        });
+    });
+}
+
+thumbnailicize(230, "thumbnails");
+thumbnailicize(400, "thumbnails_lrg");
